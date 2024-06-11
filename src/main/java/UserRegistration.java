8b5c825f740399e0b8dcd21dba360832af80a03c
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-06-11 15:59:44 +0530

    UC3 Email Validation Added

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -12,6 +12,17 @@ public class UserRegistration {
             System.out.println("Invalid Input");
         }
     }
+    public static void emailId(String name ){
+        Pattern pattern=Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+        Matcher match= pattern.matcher(name);
+        boolean ismatch= match.matches();
+        if(ismatch){
+            System.out.println("Valid input");
+        }else {
+            System.out.println("Inavaid input");
+        }
+
+    }
 
     public static void LastName(String name){
         Pattern pattern = Pattern.compile("[A-Z][a-z]{3,}");
@@ -26,11 +37,14 @@ public class UserRegistration {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First  Name");
+        System.out.println("Enter First Name");
         String name =sc.nextLine();
-        System.out.println("Enter Last  Name");
-        String name1 =sc.nextLine();
-        FirstName(name);
-        LastName(name1);
+        System.out.println("Enter Last Name");
+        String lastname =sc.nextLine();
+        System.out.println("Enter Email:");
+        String email =sc.nextLine();
+        //FirstName(name);
+        //LastName(lastname);
+        emailId(email);
     }
 }
