5d021a97715c99d5faad7f08fd427904eca7d6ae
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-06-11 14:17:01 +0530

    UC2 validate last name

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -13,12 +13,24 @@ public class UserRegistration {
         }
     }
 
-
+    public static void LastName(String name){
+        Pattern pattern = Pattern.compile("[A-Z][a-z]{3,}");
+        Matcher matcher = pattern.matcher(name);
+        boolean ismatch= matcher.matches();
+        if(ismatch){
+            System.out.println("Valid input");
+        }
+        else{
+            System.out.println("Invalid Input");
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First  Name");
         String name =sc.nextLine();
+        System.out.println("Enter Last  Name");
+        String name1 =sc.nextLine();
         FirstName(name);
-
+        LastName(name1);
     }
 }
